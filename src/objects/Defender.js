92b6e72c7ac4fe63efa4cf232d2a4e07900a262a
newtonjjang <newtonjjang@naver.com>
2023-11-29 00:27:47 +0900

    [fix] 수비측 attack 함수 throttling 삭제

--- src/objects/Defender.js
+++ src/objects/Defender.js
@@ -10,20 +10,20 @@ class Defender extends Player {
 
       let pg = this.pg;
       // throttling
-      if (!this.isAttacked) {
-        pg.push();
-        pg.rectMode(pg.CENTER);
-        pg.translate(this.x, this.y);
-        pg.rotate(this.deg);
-        pg.fill(255);
-        pg.rect(0, -20, 50, 150);
-        pg.pop();
+      // if (!this.isAttacked) {
+      pg.push();
+      pg.rectMode(pg.CENTER);
+      pg.translate(this.x, this.y);
+      pg.rotate(this.deg);
+      pg.fill(255);
+      pg.rect(0, -20, this.width, this.height);
+      pg.pop();
 
-        this.isAttacked = true;
-        setTimeout(() => {
-          this.isAttacked = false;
-        }, 100);
-      }
+      this.isAttacked = true;
+      setTimeout(() => {
+        this.isAttacked = false;
+      }, 100);
+      // }
 
       // ink.drawCurveVertexesToPG(pg, this.x, this.y, this.deg);
       pop();
